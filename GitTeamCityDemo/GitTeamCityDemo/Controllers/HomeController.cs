8c2607a3623fbc2cef2057252921a8c332d48dd2
Kashish <kashish.kumar@nagarro.com>
2017-08-29 13:26:20 +0530

    error 2

--- GitTeamCityDemo/GitTeamCityDemo/Controllers/HomeController.cs
+++ GitTeamCityDemo/GitTeamCityDemo/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace GitTeamCityDemo.Controllers
         {
             ViewBag.Message = "Your application description page.";
 
-            return View();
+            //return View();
         }
 
         public ActionResult Contact()
